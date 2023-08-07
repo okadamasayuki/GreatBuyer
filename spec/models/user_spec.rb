e74537c6178c21da84157027fc9b094707f2f2c4
okadamasayuki <wrvnfewr@gmail.com>
2023-08-07 23:36:51 +0900

    rspecにテストを書きました。

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.new(name: 'Example User', email: 'rspec_test@example.com', password: 'password') }
+ 
+  it 'userが有効であること' do
+    expect(user).to be_valid
+  end
 end
