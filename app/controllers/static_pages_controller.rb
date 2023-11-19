df3ecac721b0f4eda40a6982af0c8b303298646e
okadamasayuki <wrvnfewr@gmail.com>
2023-11-20 01:34:07 +0900

    他ユーザのUIを整えました。

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -32,7 +32,24 @@ class StaticPagesController < ApplicationController
 
     def one_friend
         @display_user = User.find(params[:display_user_id])
-        @posts = @display_user.posts.order(created_at: :desc).paginate(page: params[:page], per_page: 15)
+        @all_posts = @display_user.posts.order(created_at: :desc)
+        @discount_sum = @all_posts.sum(:discount)
+        @posts_in_this_page = @all_posts.paginate(page: params[:page], per_page: 10)
+        this_month_post = @all_posts.where(created_at: Date.today.beginning_of_month..Date.today.end_of_month)
+        prev_month_post = @all_posts.where(created_at: 1.months.ago.all_month)
+        prev_2_month_post = @all_posts.where(created_at: 2.months.ago.all_month)
+        prev_3_month_post = @all_posts.where(created_at: 3.months.ago.all_month)
+        prev_4_month_post = @all_posts.where(created_at: 4.months.ago.all_month)
+        @data = [
+                    [Date.today.month.to_s+"月", this_month_post.sum(:discount)],
+                    [Date.today.months_ago(1).month.to_s+"月", prev_month_post.sum(:discount)],
+                    [Date.today.months_ago(2).month.to_s+"月", prev_2_month_post.sum(:discount)],                    [Date.today.months_ago(1).month.to_s+"月", prev_month_post.sum(:discount)],
+                    [Date.today.months_ago(3).month.to_s+"月", prev_3_month_post.sum(:discount)],                    [Date.today.months_ago(1).month.to_s+"月", prev_month_post.sum(:discount)],
+                    [Date.today.months_ago(4).month.to_s+"月", prev_4_month_post.sum(:discount)],
+                ]
+        pie_chart_data = @all_posts.group(:shop).sum(:discount)
+        @pie_chart_data = pie_chart_data.sort_by {|key, val| -val }.to_h
+        @pie_chart_data.delete(nil)
     end
 
 end
