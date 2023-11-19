208f5ae88474e30b41fcffa38b5921d608ce6caf
okadamasayuki <wrvnfewr@gmail.com>
2023-11-19 16:58:17 +0900

    写真が投稿されたときに違和感のないようなCSSに修正しました。

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,10 +8,14 @@ class StaticPagesController < ApplicationController
         this_month_post = posts.where(created_at: Date.today.beginning_of_month..Date.today.end_of_month)
         prev_month_post = posts.where(created_at: 1.months.ago.all_month)
         prev_2_month_post = posts.where(created_at: 2.months.ago.all_month)
+        prev_3_month_post = posts.where(created_at: 3.months.ago.all_month)
+        prev_4_month_post = posts.where(created_at: 4.months.ago.all_month)
         @data = [
                     [Date.today.month.to_s+"月", this_month_post.sum(:discount)],
                     [Date.today.months_ago(1).month.to_s+"月", prev_month_post.sum(:discount)],
-                    [Date.today.months_ago(2).month.to_s+"月", prev_2_month_post.sum(:discount)],
+                    [Date.today.months_ago(2).month.to_s+"月", prev_2_month_post.sum(:discount)],                    [Date.today.months_ago(1).month.to_s+"月", prev_month_post.sum(:discount)],
+                    [Date.today.months_ago(3).month.to_s+"月", prev_3_month_post.sum(:discount)],                    [Date.today.months_ago(1).month.to_s+"月", prev_month_post.sum(:discount)],
+                    [Date.today.months_ago(4).month.to_s+"月", prev_4_month_post.sum(:discount)],
                 ]
 
     end
