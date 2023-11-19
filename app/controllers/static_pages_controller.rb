92e2927eaae9bdf3cf54c1301ce77f27488f0217
okadamasayuki <wrvnfewr@gmail.com>
2023-11-20 02:06:37 +0900

    ユーザ一覧のUIを整えました

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -23,7 +23,7 @@ class StaticPagesController < ApplicationController
     end
 
     def find_friend
-        @users = User.paginate(page: params[:page], per_page: 15)
+        @users = User.paginate(page: params[:page], per_page: 35)
     end
 
     def friends
